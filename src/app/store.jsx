c2ba7ece41b1a09d721bddd26f1e8d4c501191fe
KSHITIJ HIMANSHU <45031325+kshitij7320@users.noreply.github.com>
2026-09-25 12:33:42 +0530

    Initial-Setup

---
+++ src/app/store.jsx
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+let store = configureStore({
+    reducer: (),
+})
---
+++ src/features/auth/state/auth/authSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        employee: null,
+        isLoading: false,
+    },
+    reducers:{
+        addEmployee: (state, action) => {
+            state.employee = action.payload;
+            state.isLoading = false;
+        },
+        removeEmployee: (state) => {
+            state.employee = null;
+            state.isLoading = false;
+        }
+    }
+})
